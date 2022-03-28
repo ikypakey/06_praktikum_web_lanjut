8d483216bb7e24600fa8c96cc8ab03345f927425
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-28 09:37:03 +0700

    create model mahasiswa

---
+++ database/migrations/2022_03_28_023237_create_mahasiswas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->string('nim',10)->primary();
+            $table->string('nama',50);
+            $table->timestamp('kelas',10);
+            $table->string('jurusan',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
